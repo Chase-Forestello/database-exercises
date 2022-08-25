1bf7d9550fd11e00802a34dd6aa79d959b8ad271
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-08-25 11:05:56 -0500

    Complete - MySQL - Tables Exercise

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS quotes;
+CREATE TABLE quotes
+(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50),
+    name VARCHAR(50),
+    release_date DATE,
+    sales FLOAT,
+    genre VARCHAR(25),
+    PRIMARY KEY (id)
+);
