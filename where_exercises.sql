28a98c522a45d1bbefd14b44f082c33c7e429d5b
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-08-30 12:25:26 -0500

    Complete - MySQL - Where Exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -10,7 +10,8 @@ WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 # All male employees named Irena, Vidya, or Maya using OR
 SELECT *
 FROM employees
-WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
+WHERE gender = 'M'
+  AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
 
 # All employees who's last name begins with 'E'
 SELECT *
@@ -20,7 +21,8 @@ WHERE last_name LIKE 'E%';
 # All employees who's last name begins and ends with 'E'
 SELECT *
 FROM employees
-WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E';
 
 # All employees with a 'Q' in their last name
 SELECT *
@@ -30,4 +32,5 @@ WHERE last_name LIKE '%Q%';
 # All employees with a 'Q' in their last name but not containing 'qu'
 SELECT *
 FROM employees
-WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%QU%';
+WHERE last_name LIKE '%Q%'
+  AND last_name NOT LIKE '%QU%';
