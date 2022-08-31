baf2a85cb524032e8d2b8a4601dd09b441fe68f0
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-08-31 16:20:41 -0500

    Incomplete - MySQL - Joins Exercises - YIKES

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,1 +1,8 @@
 # -- Incomplete - MySQL - Joins Exercises
+
+use employees;
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+         JOIN dept_manager as de
+              ON de.emp_no = employees.emp_no;
