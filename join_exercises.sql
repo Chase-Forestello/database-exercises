4d47cbb380715d3c24f04af9f90b4d4d7cd23a56
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-09-02 11:52:37 -0500

    Complete - MySQL - Joins Exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -31,27 +31,28 @@ ORDER BY count(titles.emp_no) desc;
 
 
 SELECT distinct d.dept_name                            AS Department_Name,
-                CONCAT(dm.emp_no) AS Department_Manager,
+                CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager,
                 s.salary                               AS Salary
 FROM employees as e
          JOIN dept_manager as dm
               ON dm.emp_no = e.emp_no
-         LEFT JOIN departments as d
+         JOIN departments as d
                    ON d.dept_no = dm.dept_no
-          LEFT JOIN salaries s on dm.emp_no = s.emp_no
+         JOIN salaries s on dm.emp_no = s.emp_no
 WHERE dm.to_date = '9999-01-01'
   AND s.to_date = '9999-01-01'
 group by d.dept_name;
 
 # BONUS
 
-# SELECT CONCAT(e.first_name, ' ', e.last_name), d.dept_name
-# FROM employees AS e
-# join dept_emp de on e.emp_no = de.emp_no
-# join departments d on de.dept_no = d.dept_no
-# join dept_manager mg on de.emp_no = mg.emp_no
-# WHERE de.to_date = '9999-01-01';
+SELECT CONCAT(e.first_name, ' ', e.last_name) as Employee, d.dept_name AS Department, CONCAT(e2.first_name,' ', e2.last_name) AS Manager
+FROM employees AS e
+join dept_emp de on e.emp_no = de.emp_no
+join departments d on de.dept_no = d.dept_no
+join employees e2 on de.emp_no = e2.emp_no
+Where de.to_date = '9999-01-01';
 
+# Query for finding employee name based on emp_no
 # SELECT CONCAT(e.first_name, ' ', e.last_name)
 # FROM employees e
-# WHERE e.emp_no = 111692
+# WHERE e.emp_no = 43624
