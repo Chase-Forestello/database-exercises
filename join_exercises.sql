f0a822964d6463b1021a42f073a51a87b8316ca0
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-09-02 13:36:35 -0500

    Complete - MySQL - Joins Exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -2,6 +2,7 @@
 
 use employees;
 
+# Department name and current manager
 SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name
 FROM employees as e
          JOIN dept_manager as dm
@@ -10,7 +11,7 @@ FROM employees as e
               ON d.dept_no = dm.dept_no
 WHERE dm.to_date = '9999-01-01';
 
-
+# Department name and current manager if they are female
 SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name
 FROM employees as e
          JOIN dept_manager as dm
@@ -20,16 +21,16 @@ FROM employees as e
 WHERE dm.to_date = '9999-01-01'
   AND e.gender = 'F';
 
-
+# Count of how many employees hold each title currently
 SELECT title, count(titles.emp_no)
 FROM titles
          join dept_emp de on titles.emp_no = de.emp_no
-where titles.to_date = '9999-01-01'
-  AND dept_no = 'd009'
+where dept_no = 'd009'
+  AND titles.to_date = '9999-01-01' AND de.to_date = '9999-01-01'
 GROUP BY title
 ORDER BY count(titles.emp_no) desc;
 
-
+# Salary of all current managers
 SELECT distinct d.dept_name                            AS Department_Name,
                 CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager,
                 s.salary                               AS Salary
@@ -45,6 +46,7 @@ group by d.dept_name;
 
 # BONUS
 
+# name and manager of all current employees
 SELECT CONCAT(e.first_name, ' ', e.last_name) as Employee, d.dept_name AS Department, CONCAT(e2.first_name,' ', e2.last_name) AS Manager
 FROM employees AS e
 join dept_emp de on e.emp_no = de.emp_no
